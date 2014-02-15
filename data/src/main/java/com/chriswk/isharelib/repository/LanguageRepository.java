3e07846a849c6735255700ac0c056a90f8035bc5
Christopher Kolstad <christopher.kolstad@gmail.com>
2014-02-15 19:03:10 +0100

    Trying to generalize graph plugin

---
+++ data/src/main/java/com/chriswk/isharelib/repository/LanguageRepository.java
@@ -0,0 +1,7 @@
+package com.chriswk.isharelib.repository;
+
+import com.chriswk.isharelib.data.Language;
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+public interface LanguageRepository extends GraphRepository<Language> {
+}
